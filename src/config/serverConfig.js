ea2196571765a75e2054ff549e4c6f148a160731
Pratyush Biswal <biswalpratyush40@gmail.com>
2025-07-19 13:19:41 +0530

    added cloudinary config

--- src/config/serverConfig.js
+++ src/config/serverConfig.js
@@ -8,4 +8,7 @@ module.exports = {
   DB_URI: process.env.mongoDB_URL,
   JWT_SECRET: process.env.JWT_SECRET,
   JWT_EXPIRY: process.env.JWT_EXPIRY,
+  cloud_name: process.env.cloud_name,
+  api_key: process.env.api_key,
+  api_secret: process.env.api_secret,
 };
